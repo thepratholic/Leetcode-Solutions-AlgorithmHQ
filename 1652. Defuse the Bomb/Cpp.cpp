61a1b31c14d28c97d98c71eacb2bd02290a00f00
Sundaram Agnihotri <110082729+Sundaram-Agnihotri@users.noreply.github.com>
2024-11-20 13:21:02 +0530

    rewrite the code follow format

---
+++ 1652. Defuse the Bomb/Cpp.cpp
@@ -0,0 +1,31 @@
+class Solution {
+    public int[] decrypt(int[] code, int k) {
+        int n = code.length;
+        int []ans = new int[n];
+
+        if(k==0)
+        return ans;
+
+        int start = 1;
+        int end = k;
+        int sum = 0;
+
+        if(k<0){
+            start = n - Math.abs(k);
+            end = n-1;
+        }
+
+        for(int i=start;i<=end;i++){
+            sum+=code[i];
+        }
+
+        for(int i=0;i<n;i++){
+            ans[i] = sum;
+            sum -= code[start %n];
+            sum += code[(end+1)%n];
+            start++;
+            end++;
+        }
+        return ans;
+    }
+}
