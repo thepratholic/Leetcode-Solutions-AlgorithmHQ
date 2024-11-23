16e0ab4d634d2626a3139f51a962c8c28ac56d06
Sundaram Agnihotri <110082729+Sundaram-Agnihotri@users.noreply.github.com>
2024-11-23 11:41:14 +0530

    write the brief approach

--- 2563. Count the Number of Fair Pairs/Cpp.cpp
+++ 2563. Count the Number of Fair Pairs/Cpp.cpp
@@ -1,6 +1,18 @@
 Contributed by : Sundaram Agnihotri (Student)
 Linkedin : https://www.linkedin.com/in/sundaram-agnihotri/
 
+Approah : 
+
+1 : Sort the array nums to facilitate efficient range queries using binary search.
+
+2 : For each element nums[i], consider it as the first element of a pair.
+
+3 : Use binary search to find:
+  a : low: The first index where the sum nums[i]+nums[j]≥lower.
+  b : up: The first index where nums[i]+nums[j]>upper.
+
+4 : Add the number of indices in the range [low, up) to the result.
+
 class Solution {
 public:
     long long countFairPairs(vector<int>& nums, int lower, int upper) {
