98adca97d26629c791c3d917f97a1ffd47cb9e69
Sundaram Agnihotri <110082729+Sundaram-Agnihotri@users.noreply.github.com>
2024-11-23 11:28:32 +0530

    write the brief approach

--- 1072. Flip Columns For Maximum Number of Equal Rows/Cpp.cpp
+++ 1072. Flip Columns For Maximum Number of Equal Rows/Cpp.cpp
@@ -1,6 +1,18 @@
 Cotriuted by : Sundaram Agnihotri
 linkedin : https://www.linkedin.com/in/sundaram-agnihotri/
 
+Approach : 
+
+1 : For each row, determine its "pattern" relative to the first element:
+   
+  a : If an element matches the first element, record 'T' (True).
+  b : Otherwise, record 'F' (False).
+
+2 : This pattern represents how the row can be flipped into a row with all equal values
+3 : Use a hash map to store the frequency of each unique pattern across all rows.
+4 : The maximum frequency of any pattern in the hash map gives the maximum number of rows that can have all values equal after flipping columns.
+5 : Return the highest frequency as the result.
+
 #include <vector>
 #include <string>
 #include <unordered_map>
