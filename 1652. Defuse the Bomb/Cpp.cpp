51f0830195cfc52170b703ddc8b292a5744ed969
Sundaram Agnihotri <110082729+Sundaram-Agnihotri@users.noreply.github.com>
2024-11-23 11:15:23 +0530

    write the brief approach

--- 1652. Defuse the Bomb/Cpp.cpp
+++ 1652. Defuse the Bomb/Cpp.cpp
@@ -1,6 +1,25 @@
 Contributed by : Sundaram Agnihotri (Student)
 Linkedin : https://www.linkedin.com/in/sundaram-agnihotri/
 
+// Approach : 
+
+// 1 : If k=0: Replace every element with 
+// 0(return an array of zeroes).
+
+// 2 : If k>0: Replace each element with the sum of the next k elements.
+
+// 3 : If k<0: Replace each element with the sum of the previous ∣k∣ elements.
+// Sliding Window Technique:
+
+// 4 : Use a window of size ∣k∣ to calculate the sum for the first element.
+
+// 5 : Slide the window across the array:
+//      a : Subtract the element leaving the window.
+//      b : Add the element entering the window.
+
+// 6 : Use modulo arithmetic to handle the circular nature of the array.
+    
+
 class Solution {
     public int[] decrypt(int[] code, int k) {
         int n = code.length;
