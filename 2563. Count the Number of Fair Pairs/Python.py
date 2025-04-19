7d9a743c3b3a248657f2d4e0b3fdb148e256bf97
thepratholic <chelaramanipratham@gmail.com>
2025-04-19 10:06:48 +0530

    19/4/25

--- 2563. Count the Number of Fair Pairs/Python.py
+++ 2563. Count the Number of Fair Pairs/Python.py
@@ -4,23 +4,33 @@
 from typing import List
 
 class Solution:
-    def lower_bound(self, nums, value):
-        left, right = 0, len(nums) - 1
-        res = 0
+    def countFairPairs(self, nums: List[int], lower: int, upper: int) -> int:
+        n = len(nums)
+        nums.sort()  # Sort the array to apply two-pointer approach
 
-        # Use two-pointer technique to count pairs with sum < value.
+        # Count pairs where sum <= upper
+        left, right = 0, n - 1
+        cnt_within_upper = 0
+
         while left < right:
-            sum = nums[left] + nums[right]
-            if sum < value:
-                # All pairs between `left` and `right` have a sum < value.
-                res += (right - left)
+            if nums[left] + nums[right] <= upper:
+                # All values from left to right-1 with nums[right] will also be valid
+                cnt_within_upper += (right - left)
                 left += 1
             else:
                 right -= 1
-        return res
 
-    def countFairPairs(self, nums: List[int], lower: int, upper: int) -> int:
-        nums.sort()
-        # Count pairs in the range [lower, upper] using `lower_bound`.
-        # `upper + 1` ensures pairs with sum <= upper are included.
-        return self.lower_bound(nums, upper + 1) - self.lower_bound(nums, lower)
+        # Count pairs where sum < lower
+        left, right = 0, n - 1
+        cnt_below_lower = 0
+
+        while left < right:
+            if nums[left] + nums[right] < lower:
+                # All values from left to right-1 with nums[right] will be invalid (too small)
+                cnt_below_lower += (right - left)
+                left += 1
+            else:
+                right -= 1
+
+        # Final result = pairs within [lower, upper] range
+        return cnt_within_upper - cnt_below_lower
