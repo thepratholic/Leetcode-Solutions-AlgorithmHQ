59f540a4037d324a0bb3acdef05e2bca0c4143ea
thepratholic <chelaramanipratham@gmail.com>
2025-05-13 09:48:58 +0530

    13/5/25

---
+++ 3335. Total Characters in String After Transformations I/Python.py
@@ -0,0 +1,22 @@
+class Solution:
+    def lengthAfterTransformations(self, s: str, t: int) -> int:
+        MOD = 10**9 + 7
+        mpp = [0] * 26
+        for c in s:
+            mpp[ord(c) - ord('a')] += 1
+
+        for _ in range(t):
+            temp = [0] * 26
+            for i in range(26):
+                f = mpp[i] % MOD
+
+                if i != 25:
+                    temp[(i + 1) % 26] = (temp[(i + 1) % 26] + f) % MOD
+                
+                else:
+                    temp[0] = (temp[0] + f) % MOD
+                    temp[1] = (temp[1] + f) % MOD
+
+            mpp = temp
+
+        return sum(mpp) % MOD
