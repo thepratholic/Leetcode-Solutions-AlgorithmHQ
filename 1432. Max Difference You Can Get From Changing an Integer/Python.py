9cc6fe7a3d87043ebba38b83cc9ccc2a05a1361c
thepratholic <chelaramanipratham@gmail.com>
2025-06-15 13:30:21 +0530

    15/6/25

---
+++ 1432. Max Difference You Can Get From Changing an Integer/Python.py
@@ -0,0 +1,22 @@
+class Solution:
+    def maxDiff(self, num: int) -> int:
+        maxi, mini = str(num), str(num)
+
+        for digit in maxi:
+            if digit != "9":
+                maxi = maxi.replace(digit, "9")
+                break
+
+
+        for i, d in enumerate(mini):
+            if i == 0:
+                if d != "1":
+                    mini = mini.replace(d, "1")
+                    break
+
+            else:
+                if d != "0" and d != mini[0]:
+                    mini = mini.replace(d, "0")
+                    break
+
+        return int(maxi) - int(mini)
