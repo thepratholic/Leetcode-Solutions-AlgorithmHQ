c59a764b50c1cb29eb7893eaeee908d0cce0d266
thepratholic <chelaramanipratham@gmail.com>
2025-06-24 12:02:13 +0530

    24/6/25

---
+++ 2200. Find All K-Distant Indices in an Array/Python.py
@@ -0,0 +1,14 @@
+from typing import List
+
+
+class Solution:
+    def findKDistantIndices(self, nums: List[int], key: int, k: int) -> List[int]:
+        ans = set()
+        n = len(nums)
+
+        for i in range(n):
+            for j in range(n):
+                if abs(i - j) <= k and nums[j] == key:
+                    ans.add(i)
+
+        return sorted(ans)
