3f33db533f850d7182d2535748cd0265f9408847
thepratholic <chelaramanipratham@gmail.com>
2025-08-29 13:39:10 +0530

    29/8/25

---
+++ 3021. Alice and Bob Playing Flower Game/Python.py
@@ -0,0 +1,7 @@
+# Contributed by Pratham Chelaramani (Student)
+# LinkedIn: https://www.linkedin.com/in/pratham-chelaramani-a44283227/
+
+
+class Solution:
+    def flowerGame(self, n: int, m: int) -> int:
+        return (n * m) // 2 # simple maths observation
